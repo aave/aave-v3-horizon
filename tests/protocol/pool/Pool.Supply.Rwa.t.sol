930dda582b21712a94f3fe72a181857bc8aaba98
YBM <yan@avara.xyz>
2025-05-12 15:39:32 -0500

    test: more scenarios around supplyWIthPermit

--- tests/protocol/pool/Pool.Supply.Rwa.t.sol
+++ tests/protocol/pool/Pool.Supply.Rwa.t.sol
@@ -96,7 +96,7 @@ contract PoolSupplyRwaTests is TestnetProcedures {
     assertEq(IAToken(aBuidl).scaledBalanceOf(alice), scaledBalanceTokenBase + supplyAmount);
   }
 
-  // supply with permit for themselves
+  // supply with permit for themself
   function test_supplyWithPermit(
     uint128 userPk,
     uint128 supplyAmount,
@@ -149,9 +149,59 @@ contract PoolSupplyRwaTests is TestnetProcedures {
     assertEq(IAToken(aBuidl).scaledBalanceOf(user), supplyAmount);
   }
 
+  // fuzz - supply with permit for themself fails if onBehalfOf is a different address
+  function test_fuzz_supplyWithPermit_should_revert_with_onBehalfOfNotSupported(
+    uint128 userPk,
+    uint128 supplyAmount,
+    uint128 underlyingBalance,
+    address onBehalfOf
+  ) public {
+    vm.assume(userPk != 0);
+    vm.assume(supplyAmount != 0 && supplyAmount <= underlyingBalance);
+    address user = vm.addr(userPk);
+    vm.assume(user != alice); // user is not alice so that they can be authorized to hold buidl first
+    vm.assume(onBehalfOf != user);
+
+    vm.startPrank(poolAdmin);
+    buidl.authorize(user, true);
+    buidl.mint(user, underlyingBalance);
+    vm.stopPrank();
+
+    EIP712SigUtils.Permit memory permit = EIP712SigUtils.Permit({
+      owner: user,
+      spender: address(contracts.poolProxy),
+      value: supplyAmount,
+      nonce: 0,
+      deadline: block.timestamp + 1 days
+    });
+    bytes32 digest = EIP712SigUtils.getTypedDataHash(
+      permit,
+      bytes(TestnetERC20(tokenList.buidl).name()),
+      bytes('1'),
+      tokenList.buidl
+    );
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPk, digest);
+
+    // permit succeeds, but reverts due to onBehalfOf
+    vm.expectRevert(bytes(Errors.ON_BEHALF_OF_NOT_SUPPORTED));
+
+    vm.prank(user);
+    contracts.poolProxy.supplyWithPermit(
+      tokenList.buidl,
+      supplyAmount,
+      onBehalfOf,
+      0,
+      permit.deadline,
+      v,
+      r,
+      s
+    );
+  }
+
   // supply with permit fails if relayer is not user which signed the tx
-  function test_supplyWithPermit_should_revert_with_relayer() public {
-    test_fuzz_supplyWithPermit_should_revert_with_relayer({
+  function test_supplyWithPermit_with_relayer_should_revert_with_onBehalfOfNotSupported() public {
+    test_fuzz_supplyWithPermit_with_relayer_should_revert_with_onBehalfOfNotSupported({
       userPk: 0x1,
       supplyAmount: 1e6,
       underlyingBalance: 1e6,
@@ -160,7 +210,7 @@ contract PoolSupplyRwaTests is TestnetProcedures {
   }
 
   // fuzz - supply with permit fails if relayer is not user which signed the tx
-  function test_fuzz_supplyWithPermit_should_revert_with_relayer(
+  function test_fuzz_supplyWithPermit_with_relayer_should_revert_with_onBehalfOfNotSupported(
     uint128 userPk,
     uint128 supplyAmount,
     uint128 underlyingBalance,
@@ -198,13 +248,69 @@ contract PoolSupplyRwaTests is TestnetProcedures {
 
     (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPk, digest);
 
+    // permit fails gracefully, but reverts due to onBehalfOf
     vm.expectRevert(bytes(Errors.ON_BEHALF_OF_NOT_SUPPORTED));
 
     vm.prank(relayer);
     contracts.poolProxy.supplyWithPermit(
       tokenList.buidl,
       supplyAmount,
-      user,
+      user, // onBehalfOf does not match msg.sender
+      0,
+      permit.deadline,
+      v,
+      r,
+      s
+    );
+  }
+
+  // fuzz - supply with permit fails gracefully
+  // function still reverts because relayer is not authorized to hold RWA
+  function test_fuzz_supplyWithPermit_fails_gracefully(
+    uint128 userPk,
+    uint128 supplyAmount,
+    uint128 underlyingBalance,
+    address relayer
+  ) public {
+    vm.assume(userPk != 0);
+    vm.assume(supplyAmount != 0 && supplyAmount <= underlyingBalance);
+    address user = vm.addr(userPk);
+    vm.assume(user != alice); // user is not alice so that they can be authorized to hold buidl first
+    vm.assume(relayer != user && relayer != poolAdmin && relayer != address(0));
+
+    vm.startPrank(poolAdmin);
+    buidl.authorize(user, true);
+    buidl.mint(user, underlyingBalance);
+    vm.stopPrank();
+
+    vm.prank(relayer);
+    buidl.approve(report.poolProxy, UINT256_MAX);
+
+    EIP712SigUtils.Permit memory permit = EIP712SigUtils.Permit({
+      owner: user,
+      spender: address(contracts.poolProxy),
+      value: supplyAmount,
+      nonce: 0,
+      deadline: block.timestamp + 1 days
+    });
+    bytes32 digest = EIP712SigUtils.getTypedDataHash(
+      permit,
+      bytes(TestnetERC20(tokenList.buidl).name()),
+      bytes('1'),
+      tokenList.buidl
+    );
+
+    (uint8 v, bytes32 r, bytes32 s) = vm.sign(userPk, digest);
+
+    // permit fails gracefully, but still reverts due to RWA authorization through normal supply flow
+    // relayer is not authorized to hold RWA
+    vm.expectRevert();
+
+    vm.prank(relayer);
+    contracts.poolProxy.supplyWithPermit(
+      tokenList.buidl,
+      supplyAmount,
+      relayer,
       0,
       permit.deadline,
       v,
@@ -221,6 +327,7 @@ contract PoolSupplyRwaTests is TestnetProcedures {
     vm.assume(userPk != 0);
     vm.assume(supplyAmount != 0 && supplyAmount <= underlyingBalance);
     address user = vm.addr(userPk);
+    vm.assume(user != alice);
 
     vm.startPrank(poolAdmin);
     buidl.authorize(user, true);
@@ -261,7 +368,7 @@ contract PoolSupplyRwaTests is TestnetProcedures {
     assertEq(IAToken(aBuidl).scaledBalanceOf(user), supplyAmount);
   }
 
-  function test_supplyWithPermit_should_revert_if_permit_is_less_then_supply_amount(
+  function test_supplyWithPermit_should_revert_if_permit_is_less_than_supply_amount(
     uint128 valueInPermit,
     uint128 supplyAmount
   ) public {
@@ -411,6 +518,8 @@ contract PoolSupplyRwaTests is TestnetProcedures {
   }
 
   function test_fuzz_reverts_supply_cap(address onBehalfOf) public {
+    vm.assume(onBehalfOf != aBuidl);
+
     vm.prank(poolAdmin);
     contracts.poolConfiguratorProxy.setSupplyCap(tokenList.buidl, 1);
 
