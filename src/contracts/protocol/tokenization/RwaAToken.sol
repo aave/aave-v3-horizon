bf165ca7adb719ad10afa845cfa592080ba9620b
Alexandru-Vlad Niculae <43644109+avniculae@users.noreply.github.com>
2025-05-14 12:58:24 +0300

    Update src/contracts/protocol/tokenization/RwaAToken.sol
    Co-authored-by: miguelmtz <36620902+miguelmtzinf@users.noreply.github.com>

--- src/contracts/protocol/tokenization/RwaAToken.sol
+++ src/contracts/protocol/tokenization/RwaAToken.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.10;
 
-import {IAccessControl} from 'src/contracts/dependencies/openzeppelin/contracts/IAccessControl.sol';
-import {IncentivizedERC20} from 'src/contracts/protocol/tokenization/base/IncentivizedERC20.sol';
 import {SafeCast} from 'src/contracts/dependencies/openzeppelin/contracts/SafeCast.sol';
 import {IERC20} from 'src/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
+import {IAccessControl} from 'src/contracts/dependencies/openzeppelin/contracts/IAccessControl.sol';
 import {Errors} from 'src/contracts/protocol/libraries/helpers/Errors.sol';
 import {AToken} from 'src/contracts/protocol/tokenization/AToken.sol';
+import {IncentivizedERC20} from 'src/contracts/protocol/tokenization/base/IncentivizedERC20.sol';
 import {IRwaAToken} from 'src/contracts/interfaces/IRwaAToken.sol';
 import {IPool} from 'src/contracts/interfaces/IPool.sol';
 
