35918e60b0211192144817383eb1ab5ef8c32d39
YBM <yan@avara.xyz>
2025-06-03 15:59:36 -0700

    feat: emit event in token manager

--- src/contracts/interfaces/IRwaAToken.sol
+++ src/contracts/interfaces/IRwaAToken.sol
@@ -22,8 +22,8 @@ interface IRwaAToken {
   );
 
   /**
-   * @notice Permits are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Permits are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function permit(
     address owner,
@@ -36,67 +36,67 @@ interface IRwaAToken {
   ) external;
 
   /**
-   * @notice Approvals are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Approvals are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function approve(address spender, uint256 amount) external returns (bool);
 
   /**
-   * @notice Allowances are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Allowances are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function increaseAllowance(address spender, uint256 addedValue) external returns (bool);
 
   /**
-   * @notice Allowances are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Allowances are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function decreaseAllowance(address spender, uint256 subtractedValue) external returns (bool);
 
   /**
-   * @notice Transfers are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Transfers are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function transfer(address recipient, uint256 amount) external returns (bool);
 
   /**
-   * @notice Transfers are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Transfers are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 
   /**
-   * @notice Not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function mintToTreasury(uint256 amount, uint256 index) external;
 
   /**
-   * @notice Transfers are not supported in liquidations.
-   * @dev Reverts if called.
+   * @notice Transfers are not supported in liquidations
+   * @dev Reverts if called
    */
   function transferOnLiquidation(address from, address to, uint256 value) external;
 
   /**
-   * @notice Transfers of the underlying asset are not supported for RWA aTokens.
-   * @dev Reverts if called.
+   * @notice Transfers of the underlying asset are not supported for RWA aTokens
+   * @dev Reverts if called
    */
   function transferUnderlyingTo(address target, uint256 amount) external;
 
   /**
-   * @notice Transfers an amount of aTokens between two users.
-   * @dev It checks for valid HF after the tranfer.
-   * @dev Only callable by aToken admin.
-   * @param from The address to transfer from.
-   * @param to The address to transfer to.
-   * @param amount The amount to be transferred.
-   * @return True if the transfer was successful, false otherwise.
+   * @notice Transfers an amount of aTokens between two users
+   * @dev It checks for valid HF after the tranfer
+   * @dev Only callable by aToken admin
+   * @param from The address to transfer from
+   * @param to The address to transfer to
+   * @param amount The amount to be transferred
+   * @return True if the transfer was successful, false otherwise
    */
   function authorizedTransfer(address from, address to, uint256 amount) external returns (bool);
 
   /**
-   * @notice Mints `amount` aTokens to `user`.
-   * @dev onBehalfOf must match the caller.
+   * @notice Mints `amount` aTokens to `user`
+   * @dev onBehalfOf must match the caller
    * @param caller The address performing the mint
    * @param onBehalfOf The address of the user that will receive the minted aTokens
    * @param amount The amount of tokens getting minted
